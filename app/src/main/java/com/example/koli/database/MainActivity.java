def2f927d564b03854e765f4b52bd25d73bfaf6e
Motwkel-Mohammed <kooli2015@gmail.com>
2018-11-12 08:58:44 -0800

    adding some files

--- app/src/main/java/com/example/koli/database/MainActivity.java
+++ app/src/main/java/com/example/koli/database/MainActivity.java
@@ -22,12 +22,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.net.URI;
+
 import static com.example.koli.database.R.id.count;
+import static com.example.koli.database.R.id.name;
+import static com.example.koli.database.R.id.salary;
 
 public class MainActivity extends AppCompatActivity {
 
-    HelperClass helper;
-    SQLiteDatabase db;
+    HelperClass helper ;
+    SQLiteDatabase db ;
 
     EditText name_text;
     EditText salary_text;
@@ -37,10 +41,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        helper = new HelperClass(this);
 
         // make phone un rotated
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -50,6 +55,55 @@ public class MainActivity extends AppCompatActivity {
 
         ok = (Button) findViewById(R.id.ok);
 
+        String[] projection = {
+                Contract._ID,
+                Contract.COLUMN_NAME,
+                Contract.COLUMN_SALARY};
+
+        TextView textView = (TextView) findViewById(R.id.count);
+
+        db = helper.getReadableDatabase();
+        Cursor cursor = db.query(Contract.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+        try {
+            textView.setText("there is " + cursor.getCount() + " in the table :\n");
+            db.close();
+
+            textView.append(Contract._ID + " - " +
+                    Contract.COLUMN_NAME + " - " +
+                    Contract.COLUMN_SALARY + "\n");
+
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(Contract._ID);
+            int nameColumnIndex = cursor.getColumnIndex(Contract.COLUMN_NAME);
+            int salaryColumnIndex = cursor.getColumnIndex(Contract.COLUMN_SALARY);
+
+
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                String currentSalary = cursor.getString(salaryColumnIndex);
+
+                // Display the values from each column of the current row in the cursor in the TextView
+                textView.append(("\n" + currentID + " - " +
+                        currentName + " - " +
+                        currentSalary));
+            }
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
+
+
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,9 +111,11 @@ public class MainActivity extends AppCompatActivity {
                 salary_text = (EditText) findViewById(R.id.salary);
 
 
-
                 final String name = name_text.getText().toString();
                 final String salary = salary_text.getText().toString();
+
+                helper.insertting(name, salary);
+
 
                 Intent intent = new Intent(MainActivity.this, Main_Activity2.class);
 
@@ -68,95 +124,11 @@ public class MainActivity extends AppCompatActivity {
 
                 startActivity(intent);
 
-
-
             }
         });
-
     }
-
 //_____________________________________________________________________________
 
-    private void insertPet() {
-        // Gets the database in write mode
-         db = helper.getWritableDatabase();
-
-        // Create a ContentValues object where column names are the keys,
-        // and Toto's pet attributes are the values.
-        ContentValues values = new ContentValues();
-        values.put(Contract.COLUMN_NAME, "Toto");
-        values.put(Contract.COLUMN_SALARY, "Terrier");
-
-        // Insert a new row for Toto in the database, returning the ID of that new row.
-        // The first argument for db.insert() is the pets table name.
-        // The second argument provides the name of a column in which the framework
-        // can insert NULL in the event that the ContentValues is empty (if
-        // this is set to "null", then the framework will not insert a row when
-        // there are no values).
-        // The third argument is the ContentValues object containing the info for Toto.
-        long newRowId = db.insert(Contract.TABLE_NAME, null, values);
-        Log.v("MainActivity","problem in "+newRowId);
-    }
-
-    private void displayDatabaseInfo() {
-        // Create and/or open a database to read from it
-         db = helper.getReadableDatabase();
-
-        // Define a projection that specifies which columns from the database
-        // you will actually use after this query.
-        String[] projection = {
-                Contract._ID,
-                Contract.COLUMN_NAME,
-                Contract.COLUMN_SALARY};
-
-        // Perform a query on the pets table
-        Cursor cursor = db.query(
-                Contract.TABLE_NAME,   // The table to query
-                projection,            // The columns to return
-                null,                  // The columns for the WHERE clause
-                null,                  // The values for the WHERE clause
-                null,                  // Don't group the rows
-                null,                  // Don't filter by row groups
-                null);                   // The sort order
-
-        TextView displayView = (TextView) findViewById(R.id.count);
-
-        try {
-            // Create a header in the Text View that looks like this:
-            //
-            // The pets table contains <number of rows in Cursor> pets.
-            // _id - name - breed - gender - weight
-            //
-            // In the while loop below, iterate through the rows of the cursor and display
-            // the information from each column in this order.
-            displayView.setText("The pets table contains " + cursor.getCount() + " Employee.\n\n");
-            displayView.append(Contract._ID + " - " +
-                    Contract.COLUMN_NAME + " - " +
-                    Contract.COLUMN_SALARY + " \n " );
-
-            // Figure out the index of each column
-            int idColumnIndex = cursor.getColumnIndex(Contract._ID);
-            int nameColumnIndex = cursor.getColumnIndex(Contract.COLUMN_NAME);
-            int breedColumnIndex = cursor.getColumnIndex(Contract.COLUMN_SALARY);
-
-            // Iterate through all the returned rows in the cursor
-            while (cursor.moveToNext()) {
-                // Use that index to extract the String or Int value of the word
-                // at the current row the cursor is on.
-                int currentID = cursor.getInt(idColumnIndex);
-                String currentName = cursor.getString(nameColumnIndex);
-                String currentBreed = cursor.getString(breedColumnIndex);
-
-                // Display the values from each column of the current row in the cursor in the TextView
-                displayView.append(("\n" + currentID + " - " +
-                        currentName + " - " +
-                        currentBreed + " "));}
-        } finally {
-            // Always close the cursor when you're done reading from it. This releases all its
-            // resources and makes it invalid.
-            cursor.close();
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -169,8 +141,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.insert_employee) {
-            insertPet();
-            displayDatabaseInfo();
             return false;
         }
         if (id == R.id.about_us) {
--- app/src/main/java/com/example/koli/database/Main_Activity2.java
+++ app/src/main/java/com/example/koli/database/Main_Activity2.java
@@ -6,8 +6,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main_Activity2 extends AppCompatActivity {
 
@@ -24,17 +26,17 @@ public class Main_Activity2 extends AppCompatActivity {
         final String name = intent.getStringExtra("name");
         final String salary = intent.getStringExtra("salary");
 
-            ArrayList<DisplayEmployee> item = new ArrayList<>();
-            item.add(new DisplayEmployee("Mohammed", "3500"));
-            item.add(new DisplayEmployee(name, salary));
-            item.add(new DisplayEmployee("Ahmed", "3000"));
-            item.add(new DisplayEmployee("Ali", "2500"));
+                ArrayList<DisplayEmployee> item = new ArrayList<>();
+                item.add(new DisplayEmployee("Mohamed", "3500"));
+                item.add(new DisplayEmployee(name, salary));
+                item.add(new DisplayEmployee("Ahmed", "3000"));
+                item.add(new DisplayEmployee("Ali", "2500"));
 
-            EmployeeAdapter adapter = new EmployeeAdapter(this, item);
+                EmployeeAdapter adapter = new EmployeeAdapter(this, item);
 
-            ListView listView = (ListView) findViewById(R.id.list);
+                ListView listView = (ListView) findViewById(R.id.list);
 
-            listView.setAdapter(adapter);
+                listView.setAdapter(adapter);
 
     }
 }
